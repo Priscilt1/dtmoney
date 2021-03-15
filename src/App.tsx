963f5f7674976d67537f801f89f1ab74b34c11ee
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-15 17:40:27 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import styled from 'styled-components'
-
-//criação do componente estilizavel
-const Title = styled.h1 `
-  font-size: 64px;
-  color: red;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-     <Title>Hello Word</Title> {/* componente criado para a estilização */}
+      <h1>Hiiiii word</h1>
+      <GlobalStyle />
     </div>
   )
 }
