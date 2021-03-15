29c6cae9149d8200bd075f39f6de77899ae7ec71
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-15 15:55:14 -0300

    Instalando o Styled Componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components'
+
+//criação do componente estilizavel
+const Title = styled.h1 `
+  font-size: 64px;
+  color: red;
+`
+
 export function App() {
   return (
     <div className="App">
-     <h1>Hello Word</h1>
+     <Title>Hello Word</Title> {/* componente criado para a estilização */}
     </div>
   )
 }
