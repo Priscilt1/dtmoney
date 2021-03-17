ff3f2dfde2dd360e797d663688819eb11ccb2d86
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-17 10:42:02 -0300

    Funcionamento de abrir e fechar o modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,36 @@
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 export function Header() {
+  // MODAL (usando a biblioteca React Modal)
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true) //setando que a informação do modal esta aberto
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money"/>
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>  {/* abrindo o modal */}
             Nova transação
         </button>
+        
+        {/* acrescentando o estado */}
+        <Modal 
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}  
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
       </Content>
     </Container>
   )
