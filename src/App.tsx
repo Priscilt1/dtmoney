ba78935c4d516d56e91a4b74185bebe334ff7aa0
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-17 16:23:47 -0300

    Componente: NewTransacionModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Dashboard } from "./components/Dashboard"
 import { useState } from "react"
 import Modal from 'react-modal'
 import { Header } from "./components/Header"
+import { NewTransactionModal } from "./components/NewTransactionModal"
 import { GlobalStyle } from "./styles/global"
 
 Modal.setAppElement('#root') //questao de acessibilidade
@@ -22,15 +23,13 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionsModal={handleOpenNewTransactionModal} />
-      <Dashboard />
 
-       {/* acrescentando o estado */}
-       <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}  
-        >
-          <h2>Cadastrar transação</h2>
-        </Modal>
+      <Dashboard />
+      
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       
       <GlobalStyle />
     </>
