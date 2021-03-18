4235bc6dbe0b9121d92d27d2ca254268ba913b55
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-18 14:07:18 -0300

    Funcionamento dos botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from 'react-modal'
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,6 +11,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) { //pegando as propriedades atraves da destruturação
+  //criando o estado para armazenar a informação de qual botão foi selecionado
+  const [type, setType] = useState ('deposit')
+  
   return(
     // acrescentando o estado
     <Modal 
@@ -40,19 +44,23 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         />
 
         <TransactionTypeContainer>
-          <button 
+          <RadioBox //transformando o buttom em componente
             type="button"
+            onClick={() => { setType('deposit')}} //funcao para mudar o estado do botao
+            isActive={type === 'deposit'} //propriedade que poderia ter qualquer nome 
           >
             <img src={incomeImg} alt="Entrada"/>
             <span>Entrada</span>
-          </button>
+          </RadioBox >
 
-          <button 
+          <RadioBox  
             type="button"
+            onClick={() => { setType('withdraw')}}
+            isActive={type === 'withdraw'}
           >
             <img src={outcomeImg} alt="Saída"/>
             <span>Saída</span>
-          </button>
+          </RadioBox >
 
         </TransactionTypeContainer>
 
